e8f9319036b613624e4417bccf91f53b841d25ba
Giedre <giedrejakucionyte8@gmail.com>
2019-12-12 20:00:53 +0200

    adding InRange function

--- ConsolePrograma/ConsolePrograma/Program.cs
+++ ConsolePrograma/ConsolePrograma/Program.cs
@@ -7,6 +7,8 @@ namespace ConsolePrograma
         static void Main(string[] args)
         {
             Console.WriteLine(Numeris(3,2,1));
+            Console.WriteLine(InRange(4, 2, 9));
+            Console.WriteLine(InRange(1, 6, 7));
         }
         static int Numeris(int n1, int n2, int n3)
         {
@@ -21,6 +23,10 @@ namespace ConsolePrograma
             {
                 return n3;
             }
+        }
+        static bool InRange(int number, int minNum, int maxNum)
+        {
+            return number >= minNum && number <= maxNum;
         }
     }
 }
