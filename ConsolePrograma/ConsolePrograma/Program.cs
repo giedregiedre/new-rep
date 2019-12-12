b71a653ff42a3d17784320ca4ff53a11e1284e40
Giedre <giedrejakucionyte8@gmail.com>
2019-12-12 20:40:21 +0200

    added IsPrimal

--- ConsolePrograma/ConsolePrograma/Program.cs
+++ ConsolePrograma/ConsolePrograma/Program.cs
@@ -6,9 +6,10 @@ namespace ConsolePrograma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Numeris(3,2,1));
-            Console.WriteLine(InRange(4, 2, 9));
-            Console.WriteLine(InRange(1, 6, 7));
+            //Console.WriteLine(Numeris(3,2,1));
+            //Console.WriteLine(InRange(4, 2, 9));
+            //Console.WriteLine(InRange(1, 6, 7));
+            Console.WriteLine(IsPrimal(7));
         }
         static int Numeris(int n1, int n2, int n3)
         {
@@ -27,6 +28,17 @@ namespace ConsolePrograma
         static bool InRange(int number, int minNum, int maxNum)
         {
             return number >= minNum && number <= maxNum;
+        }
+        static bool IsPrimal(int skaicius)
+        {
+            for(int i = 2; i < skaicius; i++)
+            {
+                if(skaicius >= 1 && skaicius % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
