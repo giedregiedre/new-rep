3225732b036f37c1829d6e3457adad7db9cf72a5
Giedre <giedrejakucionyte8@gmail.com>
2019-12-12 19:54:52 +0200

    skaiciuojaMaxSkaiciu

---
+++ ConsolePrograma/ConsolePrograma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsolePrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Numeris(3,2,1));
+        }
+        static int Numeris(int n1, int n2, int n3)
+        {
+            if(n1 > n2 && n1 > n2)
+            {
+                return n1;
+            }else if(n2 > n3)
+            {
+                return n2;
+            }
+            else
+            {
+                return n3;
+            }
+        }
+    }
+}
